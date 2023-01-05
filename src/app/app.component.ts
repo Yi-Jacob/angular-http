435ac765db9bfb53800730b4b94832273efacd72
Yi-Jacob <jakeyi00@gmail.com>
2023-01-04 18:56:12 -0800

    in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component, Injectable, ReflectiveInjector } from '@angular/core';
 import { WikipediaService } from './wikipedia.service';
+import { Observable } from 'rxjs';
 
+const observable = new Observable((observer) => {
+  observer.next(1)
+})
+
+observable.subscribe(x => {
+  console.log(x)
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
